c94f3b355b41024c1977a8c6c66d4a09c49309f1
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-08-19 10:02:43 +0200

    Some more of the same

---
+++ SpaceShooter/Assets/Scripts/MovingBackground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+    }
+}
