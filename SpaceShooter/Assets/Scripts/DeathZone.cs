08946b6427a86b1652634eb1634cb29fc21540a6
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-08-19 10:30:09 +0200

    Added basic elements
    -Character controller addded
    -Moving Background added
    -Deathzones added

---
+++ SpaceShooter/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Object.Destroy(other.gameObject);
+            Debug.Log("Killed outer bound");
+        }
+    }
+}
