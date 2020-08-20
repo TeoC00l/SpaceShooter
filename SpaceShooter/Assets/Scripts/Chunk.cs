d1f1d9ff1e5f6a86ba53ea03904593dae8232223
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-08-20 17:19:14 +0200

    ChunkManager and Chunk mechanics
    The ReggaeGrotto test scene now contains the latest and greatest in chunk-technology

---
+++ SpaceShooter/Assets/Scripts/Chunk.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    private ChunkManager chunkManager;
+    private float movementSpeed = 10;
+    private Vector2 startPosition = new Vector2(99.7f, 0f);
+    private float horizontalDestroyCoordinate = -50f;
+
+    void Awake()
+    {
+        transform.position = startPosition;
+    }
+
+    void Start()
+    {
+        chunkManager = FindObjectOfType<ChunkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+
+        if( transform.position.x < horizontalDestroyCoordinate)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("MainCamera")){
+            chunkManager.CreateNewChunk();
+        }
+    }
+}
