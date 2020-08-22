ae56c60d0c65a52e9893b90ba0f16eb584482efd
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-08-22 12:04:59 +0200

    nodes

--- SpaceShooter/Assets/Scripts/ChunkManager.cs
+++ SpaceShooter/Assets/Scripts/ChunkManager.cs
@@ -7,10 +7,14 @@ using Random = UnityEngine.Random;
 //TODO: is activeChunks Needed?
 public class ChunkManager : MonoBehaviour
 {
-    [SerializeField]private float chunkSpeed = 10;
+    private int nodeSpawnPercentChance = 50;
+    [SerializeField] private float chunkSpeed = 10;
     private Vector2 startPosition = new Vector2(70f, 0f);
     public float ChunkSpeed {set { chunkSpeed = value; } get { return chunkSpeed;  } }
+    
     [SerializeField] private GameObject[] chunks;
+    [SerializeField] private GameObject[] spawnObjects;
+
     private Random random = new Random();
     private List<GameObject> activeChunks = new List<GameObject>();
     private GameObject lastChunk;
@@ -23,6 +27,7 @@ public class ChunkManager : MonoBehaviour
 
     public void CreateNewChunk()
     {
+        Debug.Log("object spawned");
         GameObject chunk = Instantiate(chunks[Random.Range(0, chunks.Length)]);
 
         if(lastChunk == null)
@@ -31,11 +36,12 @@ public class ChunkManager : MonoBehaviour
         }
         else
         {
-            chunk.transform.position = calculateSpawnPoint();
+            chunk.transform.position = CalculateSpawnPoint();
 
         }
         activeChunks.Add(chunk);
         lastChunk = chunk;
+        InitializeNodes();
     }
 
     public void RemoveChunk(GameObject chunk)
@@ -44,11 +50,31 @@ public class ChunkManager : MonoBehaviour
         Destroy(chunk);
     }
 
-    public Vector3 calculateSpawnPoint()
+    private Vector3 CalculateSpawnPoint()
     {
         Vector3 offset = lastChunk.transform.right * lastChunk.transform.localScale.x;
         Vector3 pos = lastChunk.transform.position + offset;
 
         return pos;
     }
+
+    private void InitializeNodes()
+    {
+        //TODO: Object pool?
+
+        GameObject[] nodes = lastChunk.GetComponent<Chunk>().nodes;
+
+        for (int i = 0; i<nodes.Length; i++)
+        {
+            if (Random.Range(0, 100) < nodeSpawnPercentChance)
+            {
+                GameObject spawned = Instantiate(spawnObjects[Random.Range(0, spawnObjects.Length)], nodes[i].transform.position, Quaternion.identity);
+                spawned.transform.parent = lastChunk.transform;
+            }
+        }
+    }
+
+
+
+    
 }
